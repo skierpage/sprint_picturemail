1a7d1da82701d194c6219f7d3996c7ea50477b62
skierpage <info@skierpage.com>
2012-05-19 14:50:46 -0700

    prettified with sed 's/javascript://' < original_bookmarklet |   ./node_modules/uglify-js/bin/uglifyjs --beautify --indent 2 >   spm.js

--- spm.js
+++ spm.js
@@ -1,3 +1,7 @@
-javascript:WN7z=open('','Z6','width=400,height=200,scrollbars,resizable,menubar');
-DL5e=document.links;with(WN7z.document){write('<base
-target=_blank>');for(lKi=0;lKi<DL5e.length;lKi++){write(DL5e[lKi].toString().link(DL5e[lKi])+'<br><br>')};void(close())}
+WN7z = open("", "Z6", "width=400,height=200,scrollbars,resizable,menubar"), DL5e = document.links;
+
+with (WN7z.document) {
+  write("<base\ntarget=_blank>");
+  for (lKi = 0; lKi < DL5e.length; lKi++) write(DL5e[lKi].toString().link(DL5e[lKi]) + "<br><br>");
+  void close();
+};
