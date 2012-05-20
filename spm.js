ff90a8550ef7b77952cb11c7ddf257fc11a7dc48
skierpage <info@skierpage.com>
2012-05-19 18:56:04 -0700

    This version gets info from the JSON returned by http://pictures.sprintpcs.com/ajax/view/getAlbumListResults.do?sortCode=5 and presents it as a set of links... almost.

--- spm.js
+++ spm.js
@@ -1,7 +1,18 @@
-WN7z = open("", "Z6", "width=400,height=200,scrollbars,resizable,menubar"), DL5e = document.links;
+// fails on badly encoded \' var albums = albums=JSON.parse(document.body.innerHTML);
+albums=JSON.parse(document.body.innerHTML.replace(/\\'/, "'"));
+linkWin = open("", "albums", "width=400,height=200,scrollbars,resizable,menubar");
+// from http://pictures.sprintpcs.com/foo/bar?baz , returns http://pictures.sprintpcs.com
+baseURL = document.baseURI.match(/(.*?\/\/.*?)\//)[1];
 
-with (WN7z.document) {
-  write("<base\ntarget=_blank>");
-  for (lKi = 0; lKi < DL5e.length; lKi++) write(DL5e[lKi].toString().link(DL5e[lKi]) + "<br><br>");
+with (linkWin.document) {
+  write('<base href="' + baseURL + '" target="_blank">');
+  for (i = 0; i < albums.length; i++) {
+    var album = albums[i];
+    var text = album.title + ' - ' + album.coverTitle;
+    // XXX should escape " > < & in the text...
+    // only the last one of these remains in the document, the other stuff is overwritten
+    write(text.link('/ui-refresh/getMediaContainerJSON.do?componentType=mediaDetail&sortCode=17&containerID=' + album.containerID) + "<br><br>");
+    console.log(text.link('/ui-refresh/getMediaContainerJSON.do?componentType=mediaDetail&sortCode=17&containerID=' + album.containerID) + "<br><br>");
   void close();
+  }
 };
