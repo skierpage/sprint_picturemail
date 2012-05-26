540397e68ec4a42c9af6e915cf74c9124d2eaa07
skierpage <info@skierpage.com>
2012-05-26 15:20:11 -0700

    explicit window.write()s, creates a numbered list. Not bad!

--- spm.js
+++ spm.js
@@ -1,18 +1,13 @@
-// fails on badly encoded \' var albums = albums=JSON.parse(document.body.innerHTML);
-albums=JSON.parse(document.body.innerHTML.replace(/\\'/, "'"));
+albums=JSON.parse(document.body.innerHTML.replace(/\\'/, "'")); // Sprint Picture Mail incorrectly encodes ' as \'
 linkWin = open("", "albums", "width=400,height=200,scrollbars,resizable,menubar");
-// from http://pictures.sprintpcs.com/foo/bar?baz , returns http://pictures.sprintpcs.com
-baseURL = document.baseURI.match(/(.*?\/\/.*?)\//)[1];
+lw = linkWin.document;
+baseURL = document.baseURI.match(/(.*?\/\/.*?)\//)[1]; // given http://pictures.sprintpcs.com/foo/bar?baz , this returns http://pictures.sprintpcs.com
 
-with (linkWin.document) {
-  write('<base href="' + baseURL + '" target="_blank">');
-  for (i = 0; i < albums.length; i++) {
-    var album = albums[i];
-    var text = album.title + ' - ' + album.coverTitle;
-    // XXX should escape " > < & in the text...
-    // only the last one of these remains in the document, the other stuff is overwritten
-    write(text.link('/ui-refresh/getMediaContainerJSON.do?componentType=mediaDetail&sortCode=17&containerID=' + album.containerID) + "<br><br>");
-    console.log(text.link('/ui-refresh/getMediaContainerJSON.do?componentType=mediaDetail&sortCode=17&containerID=' + album.containerID) + "<br><br>");
-  void close();
-  }
+lw.write('<base href="' + baseURL + '" target="_blank">\n');
+lw.write('<ol>\n');
+for (i = 0; i < albums.length; i++) {
+  var album = albums[i];
+  // XXX should escape " > < & in the text...
+  lw.write('  <li>' + album.title.link('/ui-refresh/getMediaContainerJSON.do?componentType=mediaDetail&sortCode=17&containerID=' + album.containerID) + ' - ' + album.coverTitle + '</li>\n\n');
 };
+lw.write('</ol>\n');
