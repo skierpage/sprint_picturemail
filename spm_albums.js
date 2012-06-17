3127e7ffaef02947ad28c8ea16736b0a571c522c
skierpage <info@skierpage.com>
2012-06-17 13:07:55 -0700

    multi-line comments for easier pasting

--- spm_albums.js
+++ spm_albums.js
@@ -2,20 +2,20 @@ var linkWin = open("", "albums", "width=400,height=600,scrollbars,resizable");
 var lw = linkWin.document;
 var isJSON = true;
 try {
-  albums=JSON.parse(document.body.innerHTML.replace(/\\'/, "'")); // Sprint Picture Mail incorrectly encodes ' as \'
+  albums=JSON.parse(document.body.innerHTML.replace(/\\'/, "'")); /* Sprint Picture Mail incorrectly encodes ' as \' */
 } catch (e) {
   isJSON = false;
 }
 if (isJSON && typeof albums === 'object' && Array.isArray(albums) && albums.length > 0) {
-  baseURL = document.baseURI.match(/(.*?\/\/.*?)\//)[1]; // given http://pictures.sprintpcs.com/foo/bar?baz , this returns http://pictures.sprintpcs.com
+  baseURL = document.baseURI.match(/(.*?\/\/.*?)\//)[1]; /* given http://pictures.sprintpcs.com/foo/bar?baz , this returns http://pictures.sprintpcs.com */
 
   lw.write('<base href="' + baseURL + '" target="_blank">\n');
   lw.write('<ol>\n');
   for (i = 0; i < albums.length; i++) {
     var album = albums[i];
-    // XXX should escape " > < & in the text...
+    /* TODO should escape " > < & in the text... */
     lw.write('  <li>' + album.title.link('/ui-refresh/getMediaContainerJSON.do?componentType=mediaDetail&sortCode=17&containerID=' + album.containerID) + ' - ' + album.coverTitle + '</li>\n\n');
-  };
+  }
   lw.write('</ol>\n');
 } else {
   lw.write('<p>window contents do not appear to be Sprint Picture Mail JSON info</p>\n');
